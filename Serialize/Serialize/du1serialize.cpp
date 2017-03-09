9f038fb8f19b69a3c6e0f37bc76c2ed590ea39ba
Michal Jurčo <michal.jurco007@gmail.com>
2017-03-09 17:38:13 +0100

    C++ HW1 - added functions for loading and dumping ints and strings

--- Serialize/Serialize/du1serialize.cpp
+++ Serialize/Serialize/du1serialize.cpp
@@ -1,6 +1,118 @@
 #include "du1serialize.hpp"
 
 namespace serialize {
+	char ignore_whitespace(std::istream & in) {
+		char ch;
+		while (!in.eof()) {
+			in >> ch;
+			if (!isspace(ch)) {
+				break;
+			}
+		}
+		if (in.eof()) {
+			throw std::exception("Unexpected end of stream");
+		}
+		else {
+			return ch;
+		}
+	}
 
+	char ignore_identifier(std::istream & in) {
+		char ch;
+		while (!in.eof()) {
+			in >> ch;
+			if (ch == '"') {
+				break;
+			}
+		}
+		if (in.eof()) {
+			// throw exception
+			throw std::exception("Unexpected end of stream");
+		}
+		else {
+			return ch;
+		}
+	}
+
+	std::int_least32_t parse_int_attribute(std::istream & in) {
+		// find start of attribute name
+		char ch;
+		ch = ignore_whitespace(in);
+		if (ch == '"') {
+			// idf name not important
+			ch = ignore_identifier(in);
+			if (ch == '"') {
+				ch = ignore_whitespace(in);
+				if (ch == ':') {
+					ch = ignore_whitespace(in);
+					bool negative = false;
+					if (ch == '-') {
+						negative = true;
+						if (!in.eof()) {
+							in >> ch;
+						}
+					}
+					if (isdigit(ch)) {
+						int_least32_t value = 0;
+						value = (int)ch - 48;
+						while (!in.eof()) {
+							in >> ch;
+							if (isdigit(ch)) {
+								value *= 10;
+								value += (int)ch - 48;
+							}
+							else {
+								break;
+							}
+						}
+						if (ch == ',') {
+							if (negative) {
+								value = -value;
+							}
+							return value;
+						}
+					}
+				}
+			}
+		}
+		// invalid input, throw exception
+		throw std::exception("Incorrect syntax detected during parsing of integer attribute");
+	}
+
+	std::string parse_string_attribute(std::istream & in) {
+		// find start of attribute name
+		char ch;
+		ch = ignore_whitespace(in);
+		if (ch == '"') {
+			// idf name not important
+			ch = ignore_identifier(in);
+			if (ch == '"') {
+				ch = ignore_whitespace(in);
+				if (ch == ':') {
+					ch = ignore_whitespace(in);
+					if (ch == '"') {
+						std::string value = "";
+						while (!in.eof()) {
+							in >> ch;
+							if (ch != '"') {
+								value += ch;
+							}
+							else {
+								break;
+							}
+						}
+						if (ch == '"') {
+							ch = ignore_whitespace(in);
+							if (ch == ',') {
+								return value;
+							}
+						}
+					}
+				}
+			}
+		}
+		// invalid input, throw exception
+		throw std::exception("Incorrect syntax detected during parsing of string attribute");
+	}
 }
 
--- Serialize/Serialize/du1serialize.hpp
+++ Serialize/Serialize/du1serialize.hpp
@@ -9,21 +9,64 @@
 
 namespace serialize {
 
+	template< typename TObj>
+	struct dump_pair {
+		std::ostream & out;
+		const TObj & obj;
+	};
+
+	template< typename TObj>
+	struct load_pair {
+		std::istream & in;
+		TObj & obj;
+	};
+
+	char ignore_whitespace(std::istream & in);
+	char ignore_identifier(std::istream & in);
+	std::int_least32_t parse_int_attribute(std::istream & in);
+	std::string parse_string_attribute(std::istream & in);
+
 	// you have to implement the following functions:
 
 	template< typename P, typename T>
 	inline void dump(std::ostream & ofs, T && t)
 	{
-		// ...
+		dump_pair<T> & pair = dump_pair<T>{ofs,t};
+		ofs << "{" << std::endl;
+		P::make(pair);
+		ofs << "}";
 	}
 
 	template< typename P>
 	inline auto load(std::istream & ifs)
 	{
 		auto t = P::create();
+		auto pair = load_pair<decltype(t)>{ ifs, t };
 
-		// ...
+		// object should start with left curly brace
+		if (!ifs.eof()) {
+			char ch;
+			ifs >> ch;
+			if (ch != '{') {
+				throw std::exception("Incorrect syntax");
+			}
+		}
+		else {
+			throw std::exception("Incorrect syntax");
+		}
 
+		P::make(pair);
+		/*
+		// object should end on right curly brace
+		if (!ifs.eof()) {
+			char ch = ignore_whitespace(ifs);
+			if (ch != '}') {
+				throw std::exception("Incorrect syntax");
+			}
+		}
+		else {
+			throw std::exception("Incorrect syntax");
+		}*/
 		return t;
 	}
 	
@@ -33,31 +76,75 @@ namespace serialize {
 	using enumeration_list = std::vector< std::pair< E, std::string>>;	
 	
 	template< typename E, typename F>
-	inline void int_attribute(E && e, std::string name, F && f)
+	inline void int_attribute(dump_pair<E> & e, std::string name, F && f)
 	{
-		// ...
+		// dump name
+		e.out << "\"" << name << "\": ";
+		// dump value
+		e.out << f(&e.obj) << "," << std::endl;
+	}
+
+	template< typename E, typename F>
+	inline void int_attribute(load_pair<E> & e, std::string name, F && f)
+	{
+		auto value = parse_int_attribute(e.in);
+		f(&e.obj) = value;
 	}
 
 	template< typename E, typename F, typename S>
-	inline void int_attribute(E && e, std::string name, F && f, S && s)
+	inline void int_attribute(dump_pair<E> & e, std::string name, F && f, S && s)
+	{
+		e.out << "\"" << name << "\": ";
+		e.out << f(&e.obj) << "," << std::endl;
+	}
+
+	template< typename E, typename F, typename S>
+	inline void int_attribute(load_pair<E> & e, std::string name, F && f, S && s)
+	{
+		auto value = parse_int_attribute(e.in);
+		s(&e.obj, value);
+	}
+
+	template< typename P, typename E, typename F>
+	inline void enum_attribute(dump_pair<E> & e, std::string name, F && f)
 	{
 		// ...
 	}
 
 	template< typename P, typename E, typename F>
-	inline void enum_attribute(E && e, std::string name, F && f)
+	inline void enum_attribute(load_pair<E> & e, std::string name, F && f)
 	{
 		// ...
 	}
 
 	template< typename E, typename F>
-	inline void string_attribute(E && e, std::string name, F && f)
+	inline void string_attribute(dump_pair<E> & e, std::string name, F && f)
 	{
-		// ...
+		e.out << "\"" << name << "\": \"";		
+		e.out << f(&e.obj) << "\"," << std::endl;
+	}
+
+	template< typename E, typename F>
+	inline void string_attribute(load_pair<E> & e, std::string name, F && f)
+	{
+		auto value = parse_string_attribute(e.in);
+		f(&e.obj) = value;
 	}
 
 	template< typename P, typename E, typename F>
-	inline void struct_sequence_attribute(E && e, std::string name, F && f)
+	inline void struct_sequence_attribute(dump_pair<E> & e, std::string name, F && f)
+	{
+		e.out << "\"" << name << "\": [" << std::endl;
+		auto seq = f(&e.obj);
+		for (auto i = seq.begin(); i != seq.end(); ++i) {
+			dump<P>(e.out, *i);
+			e.out << "," << std::endl;
+		}
+		e.out << "]," << std::endl;
+	}
+
+	template< typename P, typename E, typename F>
+	inline void struct_sequence_attribute(load_pair<E> & e, std::string name, F && f)
 	{
 		// ...
 	}
