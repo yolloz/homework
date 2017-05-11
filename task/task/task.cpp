506263b0dbfdbfe355f44ef6921e5c0531b50b89
Michal Jurčo <michal.jurco007@gmail.com>
2017-05-11 17:24:58 +0200

    Task - void specialization

--- task/task/task.cpp
+++ task/task/task.cpp
@@ -1,11 +1,23 @@
+// MICHAL JURCO
+
 #include "task.hpp"
 
 #include <iostream>
 #include <chrono>
 
+/*
+	Main inlcudes tests for all member and global functions
+	First part contains tests for templated task with int and double tasks
+	Second part contains tests for void task
+	
+	To run tests, just uncommnet desired test
+	Global functions require vector section uncommented
+*/
+
 int main() {
 	// TEST copying and reevaluating tasks
-	/*auto task = Taskify::Task<int>([]() {
+	/*std::cout << std::endl << "TESTING templated copying and reevaluation\n"<< std::endl;
+	auto task = Taskify::Task<int>([]() {
 		std::this_thread::sleep_for(std::chrono::milliseconds(500));
 		std::cout << "returning 8\n";
 		return 8;
@@ -26,8 +38,8 @@ int main() {
 	std::cout << task.GetResult() << std::endl;
 	task3.GetResult();*/
 
-	// create vector for global functions
-	std::vector<Taskify::Task<double>> tasks;
+	// create vector for global functions	
+	/*std::vector<Taskify::Task<double>> tasks;
 	tasks.push_back(Taskify::Task<double>([]() {
 		double rtc = 0;
 		for (size_t i = 0; i < 10000000; i++)
@@ -44,14 +56,16 @@ int main() {
 	tasks.push_back(Taskify::Task<double>([]() {
 		std::cout << "fast task finished\n";
 		return (double)10;
-	}));
-
+	}));*/
+	
 	// TEST WaitAny
-	/*size_t i = Taskify::WaitAny(tasks);
+	/*std::cout << std::endl << "TESTING templated WaitAny\n"<< std::endl;
+	size_t i = Taskify::WaitAny(tasks);
 	std::cout << tasks[i].GetResult() << std::endl;*/
 
 	// TEST WhenAny
-	/*auto t = Taskify::WhenAny(tasks);
+	/*std::cout << std::endl << "TESTING templated WhenAny\n"<< std::endl;
+	auto t = Taskify::WhenAny(tasks);
 	std::cout << (t.HasStarted() ? "New running" : "New not running") << std::endl;
 	for (size_t i = 0; i < tasks.size(); i++)
 	{
@@ -60,19 +74,105 @@ int main() {
 	std::cout << t.GetResult() << std::endl;*/
 
 	// TEST WaitAll
-	/*Taskify::WaitAll(tasks);
+	/*std::cout << std::endl << "TESTING templated WaitAll\n"<< std::endl;
+	Taskify::WaitAll(tasks);
 	tasks[0].WaitTask();
 	std::cout << tasks[0].GetResult() << " " << tasks[1].GetResult() << std::endl;*/
 
 	// TEST WhenAll
+	/*std::cout << std::endl << "TESTING templated WHenAll\n"<< std::endl;
 	auto t = Taskify::WhenAll(tasks);
 	std::cout << (t.HasStarted() ? "New running" : "New not running") << std::endl;
 	auto result = t.GetResult();
 	for (size_t i = 0; i < result.size(); i++)
 	{
 		std::cout << i << ":" << result[i] << std::endl;
-	}
+	}*/
 
 	// TEST StartNew
-	auto task4 = Taskify::StartNew<void>([]() {std::cout << "Start new function test\n"; return 3; });	
+	/*std::cout << std::endl << "TESTING StartNew\n" << std::endl;
+	auto task4 = Taskify::StartNew<void>([]() {std::cout << "Start new function test\n"; return 3; });*/	
+
+
+
+	///*** VOID TEST ***///
+
+	// TEST copying and reevaluating tasks
+	std::cout << std::endl << "TESTING void copying and reevaluating\n" << std::endl;
+	auto task1 = Taskify::Task<void>([]() {
+	std::cout << "task 1 starting\n";
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	std::cout << "task 1 finishing\n";
+	});
+	auto task2 = task1.ContinueWith<void>([](){
+	std::cout << "task 2 starting\n";
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	std::cout << "task 2 finishing\n";
+	});
+
+	auto task3 = Taskify::Task<void>([]() {
+	std::cout << "task 3 starting\n";
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+	std::cout << "task 2 finishing\n";
+	});
+	task3.Start();
+	std::cout << "task 3 started\n";
+	task2.GetResult();
+	task1.GetResult();
+	task1.GetResult();
+	task3.GetResult();
+
+	// create vector for global functions
+	/*std::vector<Taskify::Task<void>> tasks;
+	tasks.push_back(Taskify::Task<void>([]() {
+		std::cout << "slow task started\n";
+		std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+		std::cout << "slow task finished\n";
+	}));
+	tasks.push_back(Taskify::Task<void>([]() {
+		std::cout << "fast task started\n";
+		std::cout << "fast task finished\n";
+	}));
+	tasks.push_back(Taskify::Task<void>([]() {
+		std::cout << "medium task started\n";
+		std::this_thread::sleep_for(std::chrono::milliseconds(200));
+		std::cout << "medium task finished\n";
+	}));*/
+
+	// TEST WaitAny
+	/*std::cout << std::endl << "TESTING void WaitAny\n"<< std::endl;
+	size_t i = Taskify::WaitAny(tasks);
+	std::cout << "WaitAny returned index " << i << std::endl;*/
+
+	// TEST WhenAny
+	/*std::cout << std::endl << "TESTING void WhenAny\n" << std::endl;
+	auto t = Taskify::WhenAny(tasks);
+	std::cout << (t.HasStarted() ? "New running" : "New not running") << std::endl;
+	for (size_t i = 0; i < tasks.size(); i++)
+	{
+	std::cout << i << (tasks[i].HasStarted() ? " running" : " not running") << std::endl;
+	}
+	t.GetResult();
+	std::cout << "Got result\n";*/
+
+	// TEST WaitAll
+	/*std::cout << std::endl << "TESTING void WaitAll\n" << std::endl;
+	Taskify::WaitAll(tasks);
+	tasks[0].WaitTask();
+	for (size_t i = 0; i < tasks.size(); i++)
+	{
+	tasks[i].GetResult();
+	}
+	std::cout << "Got all results\n" << std::endl;*/
+
+
+	// TEST WhenAll
+	/*std::cout << std::endl << "TESTING void WhenAll\n"<< std::endl;
+	auto t = Taskify::WhenAll(tasks);
+	std::cout << (t.HasStarted() ? "New running" : "New not running") << std::endl;
+	t.GetResult();
+	for (size_t i = 0; i < tasks.size(); i++)
+	{
+		std::cout << i << ":" << (!tasks[i].IsRunning() && tasks[i].HasStarted() ? "finished" : "not finished") << std::endl;
+	}*/
 }
--- task/task/task.hpp
+++ task/task/task.hpp
@@ -12,6 +12,7 @@
 
 namespace Taskify {
 	template<class Ret>
+	// represents a task which should be executed in a different thread with delayed start
 	class Task
 	{
 	public:
@@ -50,6 +51,7 @@ namespace Taskify {
 			_future = std::make_shared<std::shared_future<Ret>>();
 		}
 
+		// Starts the execution of task, if task is already running, nothing happens
 		void Start() {
 			if (_hasStarted->load() == false) {
 				bool exp = false;
@@ -61,13 +63,15 @@ namespace Taskify {
 				}
 			}
 		}
+
+		// Waits for the task to finish and returns result
 		Ret GetResult() {
 			if (_hasStarted->load() == false) {
 				Start();
 			}
 			return _future->get();
 		}
-
+		// Waits for the task to finish
 		void WaitTask() {
 			if (_hasStarted->load() == false) {
 				Start();
@@ -75,31 +79,36 @@ namespace Taskify {
 			_future->wait();
 		}
 
+		// Chains new task after current task, which will be executed right after current task ends
 		template<class CRet>
 		Task<CRet> ContinueWith(std::function<CRet(Ret)> f) {
 			auto task = Task<CRet>(f, *this);
 			return task;
 		}
+
 		~Task() {
 			if (_thread.joinable()) { _thread.join(); }
 		}
 
+		// Returns value indicating whether the task is running or not
 		bool IsRunning() {
 			return _isRunning->load();
 		}
 
+		// Returns value indicating whether the task has been started or not
 		bool HasStarted() {
 			return _hasStarted->load();
 		}
 	private:
-		std::function<void(std::promise<Ret>&&, std::atomic<bool>*)> _wrapper;
-		std::shared_ptr<std::shared_future<Ret>> _future;
-		std::thread _thread;
+		std::function<void(std::promise<Ret>&&, std::atomic<bool>*)> _wrapper;	// wraps tasks' function so it can be run later
+		std::shared_ptr<std::shared_future<Ret>> _future;	// future for storing result
+		std::thread _thread;	// tasks' thread
 		std::shared_ptr<std::atomic<bool>> _hasStarted;
 		std::shared_ptr<std::atomic<bool>> _isRunning;		
 	};
 
 	template<class Ret>
+	// Blocks the thread until all given tasks are finished
 	void WaitAll(std::vector<Task<Ret>>& tasks) {
 		for (size_t i = 0; i < tasks.size(); i++)
 		{
@@ -112,6 +121,7 @@ namespace Taskify {
 	}
 
 	template<class Ret>
+	// Blocks the thread until any of the given tasks finishes, returns index of finished task
 	size_t WaitAny(std::vector<Task<Ret>>& tasks) {
 		for (size_t i = 0; i < tasks.size(); i++)
 		{
@@ -129,6 +139,7 @@ namespace Taskify {
 	}
 
 	template<class Ret>
+	// Creates task that waits for all given tasks to finish and returns all their results
 	Task<std::vector<Ret>> WhenAll(std::vector<Task<Ret>>& tasks) {
 		return Task<std::vector<Ret>>([&]() {
 			for (size_t i = 0; i < tasks.size(); i++)
@@ -144,7 +155,8 @@ namespace Taskify {
 		});
 	}
 
-	template<class Ret> 
+	template<class Ret>
+	// Returns task that waits for any of the given tasks to finish and returns it's result
 	Task<Ret> WhenAny(std::vector<Task<Ret>>& tasks) {
 		return Task<Ret>([&]() {
 			for (size_t i = 0; i < tasks.size(); i++)
@@ -164,21 +176,14 @@ namespace Taskify {
 	}
 
 	template <class Ret>
+	// Creates and starts new task immediately
 	Task<Ret> StartNew(std::function<Ret(void)> f) {
 		auto task = Task<Ret>(f);
 		task.Start();
 		return task;
 	}
 
-
-
-
-
-
-
-
-
-
+	/* VOID SPECIALIZATION */
 	template<>
 	class Task<void>
 	{
@@ -194,11 +199,19 @@ namespace Taskify {
 			_hasStarted->store(false);
 			_isRunning = std::make_shared<std::atomic<bool>>();
 			_isRunning->store(false);
+			_future = std::make_shared<std::shared_future<void>>();
+		}
+
+		Task(const Task<void> & other) {
+			_wrapper = other._wrapper;
+			_future = other._future;
+			_isRunning = other._isRunning;
+			_hasStarted = other._hasStarted;
 		}
 
 		template<class PRet>
 		Task(std::function < void(PRet) > f, Task<PRet> previousTask) {
-			_wrapper = [=](std::promise<Ret>&& promise, std::atomic<bool> * running) mutable {
+			_wrapper = [=](std::promise<void>&& promise, std::atomic<bool> * running) mutable {
 				PRet previousResult = previousTask.GetResult();
 				f(previousResult);
 				promise.set_value();
@@ -207,12 +220,20 @@ namespace Taskify {
 			_hasStarted->store(false);
 			_isRunning = std::make_shared<std::atomic<bool>>();
 			_isRunning->store(false);
+			_future = std::make_shared<std::shared_future<void>>();
 		}
 
-		Task(const Task<void> & other) {
-			_wrapper = other._wrapper;
-			_isRunning = other._isRunning;
-			_hasStarted = other._hasStarted;
+		Task(std::function < void(void) > f, Task<void> previousTask) {
+			_wrapper = [=](std::promise<void>&& promise, std::atomic<bool> * running) mutable {
+				previousTask.GetResult();
+				f();
+				promise.set_value();
+			};
+			_hasStarted = std::make_shared<std::atomic<bool>>();
+			_hasStarted->store(false);
+			_isRunning = std::make_shared<std::atomic<bool>>();
+			_isRunning->store(false);
+			_future = std::make_shared<std::shared_future<void>>();
 		}
 
 		void Start() {
@@ -221,33 +242,82 @@ namespace Taskify {
 				if (_hasStarted->compare_exchange_weak(exp, true)) {
 					std::promise<void> promise;
 					auto tmp = promise.get_future();
-					_future = tmp.share();
+					*_future = tmp.share();
 					_thread = std::thread(_wrapper, std::move(promise), _isRunning.get());
 				}
 			}
 		}
+
 		void GetResult() {
 			if (_hasStarted->load() == false) {
 				Start();
 			}
-			_future.wait();
-			_future.get();
+			_future->get();
+		}
+
+		void WaitTask() {
+			if (_hasStarted->load() == false) {
+				Start();
+			}
+			_future->wait();
 		}
 
 		template<class CRet>
 		Task<CRet> ContinueWith(std::function<CRet(void)> f) {
-			auto task = Task<CRet>(f, *this);
+			Task<CRet> task = Task<CRet>(f, *this);
 			return task;
 		}
+
 		~Task() {
 			if (_thread.joinable()) { _thread.join(); }
 		}
+
+		bool IsRunning() {
+			return _isRunning->load();
+		}
+
+		bool HasStarted() {
+			return _hasStarted->load();
+		}
 	private:
 		std::function<void(std::promise<void>&&, std::atomic<bool>*)> _wrapper;
-		std::shared_future<void> _future;
+		std::shared_ptr<std::shared_future<void>> _future;
 		std::thread _thread;
 		std::shared_ptr<std::atomic<bool>> _hasStarted;
 		std::shared_ptr<std::atomic<bool>> _isRunning;
 	};
+
+	Task<void> WhenAll(std::vector<Task<void>>& tasks) {
+		return Task<void>([&]() {
+			for (size_t i = 0; i < tasks.size(); i++)
+			{
+				tasks[i].Start();
+			}
+			for (size_t i = 0; i < tasks.size(); i++)
+			{
+				tasks[i].GetResult();
+			}
+			return;
+		});
+	}
+
+	template<>
+	Task<void> WhenAny(std::vector<Task<void>>& tasks) {
+		return Task<void>([&]() {
+			for (size_t i = 0; i < tasks.size(); i++)
+			{
+				tasks[i].Start();
+			}
+			while (true) {
+				for (size_t i = 0; i < tasks.size(); i++)
+				{
+					if (tasks[i].HasStarted() && !tasks[i].IsRunning()) {
+						return;
+					}
+				}
+				std::this_thread::yield();
+			}
+		});
+	}
 }
 #endif
